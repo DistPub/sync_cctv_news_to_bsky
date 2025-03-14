9332b925b3af580cf1bade7b54a88dd09ba1d569
Smite Chow <xiaopengyou@live.com>
2025-03-14 20:24:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def fetch_news(lm_id, date):
         return
 
     news_box = []
-
+    print(json.dumps(news_data))
     for news in news_data['data']['list']:
         if news['mode'] == 0:
             continue
